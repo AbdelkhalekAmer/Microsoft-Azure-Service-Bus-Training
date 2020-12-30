2ed031095c73cc5a5cc8af88203f92f8f9a14950
Abdelkhalek Amer <Abdelkhalek.Amer@integrant.com>
2020-12-30 15:15:57 +0200

    Add project files.

---
+++ service.bus.training/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace service.bus.training
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
